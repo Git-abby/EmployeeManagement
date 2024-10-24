13e5ddedfae4b44e20bc2a14f7ac9dffc81a7c39
Git-abby <malekabrar56@gmail.com>
2024-10-24 18:14:25 -0400

    initial angular projects' commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'first_angular';
+  // Creating Array of steps to render dynamic
+  // Syntax = name of array: any [] = [data]
+  stepsList: any [] = [
+    { stepName: 'Basic Details', isCompleted: false },
+    { stepName: 'Skills', isCompleted: false },
+    { stepName: 'Experience', isCompleted: false },
+  ];
+
+  activeStep: any = this.stepsList[0];
+
+  setActiveStep(activeStep: any) {
+    this.activeStep = activeStep;
+  }
 }
